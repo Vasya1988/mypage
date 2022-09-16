adbf1459cf083be71ba659e931c739cfaf7a8b79
Василий Романов <myhappy1988@gmail.com>
2022-09-16 11:03:57 +0300

    create about me page

---
+++ src/components/aboutMe/AboutMe.js
@@ -0,0 +1,84 @@
+import classes from './AboutMe.module.css';
+
+const AboutMe = (props) => {
+    return (
+        <div
+            className={classes.AboutMe}
+        >
+            <div>
+                <div 
+                    className={classes.MyInfo}
+                >
+                    <h1><span>Romanov</span> Vasily</h1>
+                    <h2>Junior frontend developer</h2>
+                </div>
+                <div
+                    className={classes.IconsWrap}
+                >
+                    <div
+                        className={classes.IconFrame}
+                    >
+                        <span>Html</span>
+                        <img  
+                            src={props.logo.iconHtml}>
+                        </img>
+                    </div>
+                    <div
+                        className={classes.IconFrame}
+                    >
+                        <span>CSS</span>
+                        <img
+                            src={props.logo.iconCss}
+                        ></img>
+                    </div>
+                    <div
+                        className={classes.IconFrame}
+                    >
+                        <span>React</span>
+                        <img
+                            src={props.logo.iconReact}
+                        ></img>
+                    </div>
+                    <div
+                        className={classes.IconFrame}
+                    >
+                        <span>JavaScript</span>
+                        <img
+                            src={props.logo.iconJavaScript}
+                        ></img>
+                    </div>
+                    <div
+                        className={classes.IconFrame}
+                    >
+                        <span>GitHub</span>
+                        <img
+                            src={props.logo.iconGitHubTwo}
+                        ></img>
+                    </div>
+                    <div
+                        className={classes.IconFrame}
+                    >
+                        <span>Photoshop</span>
+                        <img
+                            src={props.logo.iconPhotoshop}
+                        ></img>
+                    </div>
+                    <div
+                        className={classes.IconFrame}
+                    >
+                        <span>Illustrator</span>
+                        <img
+                            src={props.logo.iconIllustrator}
+                        ></img>
+                    </div>
+                </div>
+            </div>
+            <img 
+                src={props.logo.avatar}
+                className={classes.Avatar}
+            
+            ></img>
+        </div>
+    )
+}
+export default AboutMe;
--- src/containers/MainFrame.js
+++ src/containers/MainFrame.js
@@ -1,12 +1,32 @@
 import classes from './MainFrame.module.css';
 import Header from '../components/Header/Header';
+import AboutMe from '../components/aboutMe/AboutMe';
+import { useState } from 'react';
 
 const MainFrame = () => {
 
+    const [skillsIcons, setSkillsIcons] = useState({
+        iconHtml: require('../images/logo/html_logo.png'),
+        iconCss: require('../images/logo/css_logo.png'),
+        iconReact: require('../images/logo/react_logo.png'),
+        iconJavaScript: require('../images/logo/js_logo.png'),
+        iconGitHub: require('../images/logo/github_logo.png'),
+        iconGitHubTwo: require('../images/logo/github_2_logo.png'),
+        iconPhotoshop: require('../images/logo/photoshop_logo.png'),
+        iconIllustrator: require('../images/logo/illustrator_logo.png'),
+        avatar: require('../images/logo/avatar.png')
+    })
+
     return (
-        <>
+        <div
+            className={classes.MainFrame}
+        >
             <Header />
-        </>
+            <AboutMe 
+                logo = {skillsIcons}
+            />
+        </div>
+
     )
 
 }
