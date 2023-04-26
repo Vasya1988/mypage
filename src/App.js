5d098c4c2583765f8374d69c90196125d3108561
Василий Романов <myhappy1988@gmail.com>
2023-04-26 17:00:00 +0300

    upd css dark mode continue

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import './App.css';
+import classes from './index.css'
 import MainFrame from './containers/MainFrame';
 
 function App(props) {
+  document.documentElement.classList.add('darkMode')
+  setTimeout(() => {document.documentElement.classList.remove('darkMode')}, 1000)
   return (
     <div className="App">
       <MainFrame />
