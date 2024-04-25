5c875c7061d531d21e1cbb9cb9133b754de39848
Vasya1988 <myhappy1988@gmail.com>
2024-04-25 13:06:06 +0300

    fixed previous commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,13 @@
 import classes from './Header.module.sass'
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {Link} from 'react-router-dom'
 import WeatherApp from '../weather-app/Weather-app';
 import DarkMode from '../darkMode/DarkMode';
 
 const Header = (props) => {
 
-    const [hash, setHash] = useState(window.location.hash)
+    const [hash, setHash] = useState(window.location.pathname)
+
     return (
         <header
             className={classes.Header}
@@ -15,7 +16,7 @@ const Header = (props) => {
                 className={classes.Header__nav}
                 onClick={() => setHash(window.location.hash)}
             >
-                <Link className={hash === '#/' || '/' ? 'active' : ''} to='/'>about me</Link>
+                <Link className={hash === '#/' || hash === '/' ? 'active' : ''} to='/'>about me</Link>
                 <Link className={hash === '#/works' ? 'active' : ''} to='/works'>my works</Link>
                 <Link className={hash === '#/blog' ? 'active' : ''} to='/blog'>blog</Link>
             </nav>
